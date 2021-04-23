083699728a94dfe8501394d69514763eafc29c7d
Kays <kaysin86@gmail.com>
2021-04-24 00:57:53 +0300

    added Homework 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// let userName = prompt('');
+// alert(`Hello, ${userName}! How are you?`);
+
+let userName = prompt('What is your name?', '');
+alert(`Hello, ${userName}! How are you?`);
+
+// let userName = prompt('What is your name?');
+// alert(`Hello, ` + userName + `! How are you?`);
