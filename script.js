a2c5d12065ec49855be818469652508ce43e69e4
Kays <kaysin86@gmail.com>
2021-04-24 01:24:08 +0300

    semicolon added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let userName = 'What is your name?';
 let yourName = prompt(userName);
-alert(`Hello,` + yourName + `! How are you?`)
+alert(`Hello,` + yourName + `! How are you?`);
 //alert(`Hello, ${yourName}! How are you?`);
 
 
