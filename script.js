3c836a1fb501063dc4bfe4d20e7246db28b4379d
Kays <kaysin86@gmail.com>
2021-04-24 14:17:49 +0300

    variable names changed, amendment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let question = 'What is your name?';
 let userName = prompt(question);
-alert(`Hello,` + question + `! How are you?`);
+alert(`Hello,` + userName + `! How are you?`);
 //alert(`Hello, ${question}! How are you?`);
 
 
