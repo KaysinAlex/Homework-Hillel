5b949eb4864953a734400feaf18ef0c6fa869257
Kays <kaysin86@gmail.com>
2021-04-27 00:37:09 +0300

    added homework2

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 // alert(`Hello,` + userName + `! How are you?`);
 // alert(`Hello, ${userName}! How are you?`);
 
+let operator = prompt('Введите одно из значение математического оператора + - * /');
+let firstNumber = prompt('ВВедите первое число');
+let secondNumber = prompt('Введите второе число');
+
+if (operator =='-') {
+  alert(firstNumber - secondNumber);
+}
+// else if (operator == '+') {
+// alert(firstNumber + secondNumber);
+// } else if (operator == '*') {
+// alert(firstNumber * secondNumber);
+// } else if (operator == '/') {
+// alert(firstNumber / secondNumber);
+// }
+//С помощью alert выводим результат действия со всеми операндами (Н-р "2 + 3 = 5" )
