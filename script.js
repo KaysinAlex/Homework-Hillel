73eb552f7f22cdd307b0d65e48e06f5b8238f185
Kays <kaysin86@gmail.com>
2021-04-26 19:15:31 +0300

    added paragraph

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-let question = 'What is your name?';
-let userName = prompt(question);
-alert(`Hello,` + userName + `! How are you?`);
+// let question = 'What is your name?';
+// let userName = prompt(question);
+// alert(`Hello,` + userName + `! How are you?`);
 //alert(`Hello, ${userName}! How are you?`);
+
+
+// в строго типизированых языках работать не будет
+// let а = 'add'
+// a = 50    в слаботипизированых -будет
+
+//const num = 4;
+//const str = String(num); // явное приведение типов четко говорим что это строка
+//const str = '' + num; // не явное преобразование , превратить в строку
+
+//const str = '44';
+//const num = Number(str);явное 
+//const num = +str; // не явное
+
+//const str = prompt('Put number');
+// const num = Number(prompt('Put number'))
+//const num = +prompt('Put number'); // не  явное тут лучше
+//const num = Number(str); //явное приведение
+
+// boolean 
+//const bool = true; // оператор '!' отрицания в консоли !bool  - будет false
+//const a = !bool; // работает только с boolean
+
+// const num = 12;
+//const bool = Boolean(num); явное 
+//const a = !!num; // ПЕРЕВЕДЕТ В BOOLEAN  явное
+
+  // undefined
+
+//const е = null;
+
+//const str = String(null); // переведет в строку поставив кавычки
+
+//const a = 15;
+// alert('a < 10');
+// alert('a >= 10');
+
+// if (a < 10) { // принять решение через if (если)
+    // alert('a <= 10');
+// } else {
+    // alert('a >= 10')
+// }
+
+
+
 
