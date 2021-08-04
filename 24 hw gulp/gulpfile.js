62ae0101ac200367dcfad3b74e7dbde4a363d32d
Kays <kaysin86@gmail.com>
2021-08-04 18:08:35 +0300

    add dist

--- 24 hw gulp/gulpfile.js
+++ 24 hw gulp/gulpfile.js
@@ -1,9 +1,24 @@
+const {parallel} = require('gulp');
+
+
 function hello(cb) {
     console.log('Hello world');
     cb();
 }
-
+function copyJs(cb) {
+    setTimeout(() => {
+        console.log('copy js');
+        cb();
+    }, 2000);
+}
+function copyCss(cb) {
+    setTimeout(() => {
+        console.log('copy Css');
+        cb();
+    }, 1000);
+}
 
 module.exports = {
     hello,
-}
+    build: parallel(copyJs, copyCss),
+};
