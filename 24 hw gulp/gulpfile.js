045bcac8ae99263580b3e42a5417719d539eab46
Kays <kaysin86@gmail.com>
2021-08-04 18:54:55 +0300

    add concat in copy js

--- 24 hw gulp/gulpfile.js
+++ 24 hw gulp/gulpfile.js
@@ -1,11 +1,13 @@
 const { parallel, src, dest } = require('gulp');
-
+const concat = require('gulp-concat');
 
 
 
 
 function copyJs() {
-   return src('./src/js/**/*.js').pipe(dest('./dist/js'));
+    return src('./src/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(dest('./dist/js'));
 }
 function copyCss() {
    return src('./src/css/**/*.css').pipe(dest('./dist/css'));
