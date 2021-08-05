e3e0957699fd3f82e6e57cc73ab5eec7fcf8c87d
Kays <kaysin86@gmail.com>
2021-08-05 19:19:41 +0300

    delete all and add json

--- 24 hw gulp/gulpfile.js
+++ 24 hw gulp/gulpfile.js
@@ -1,44 +1,40 @@
 const { parallel, src, dest } = require('gulp');
 const concat = require('gulp-concat'); // Объединение файлов - конкатенация
-const uglify = require('gulp-uglify'); //Минимизация javascript
-const cssNano = require('gulp-cssnano'); // Минимизация CSS
+// const uglify = require('gulp-uglify'); //Минимизация javascript
+// const cssNano = require('gulp-cssnano'); // Минимизация CSS
 
 function copyJs() {
     return src('./src/js/**/*.js')
-        .pipe(concat('index.js'))
-        .pipe(dest('./dist'));
+        .pipe(concat('app.js'))
+        .pipe(dest('./dist/js'));
 }
 function copyCss() {
     return src('./src/css/**/*.css')
-        .pipe(dest('./dist'));
+        .pipe(dest('./dist/css'));
 }
-/* function copyJqueryJs() {
-    return src('./src/jquery/jquery-1.8.3.js')
-        .pipe(dest('./dist/js/jquery'));
-} */
+
 function copyHttml() {
     return src('./src/index.html')
         .pipe(dest('./dist'));
 }
 
-function copyMiniUglifyJs() {
-    return src('./src/js/**/*.js')
-        .pipe(concat('index.js'))
-        .pipe(uglify())
-        .pipe(dest('./dist'));
-}
-function copyMiniCssNano() {
-    return src('./src/css/**/*.css')
-        .pipe(concat('app.css'))
-        .pipe(cssNano())
-        .pipe(dest('./dist'));
-}
+// function copyMiniUglifyJs() {
+//     return src('./src/js/**/*.js')
+//         .pipe(concat('index.js'))
+//         .pipe(uglify())
+//         .pipe(dest('./dist'));
+// }
+// function copyMiniCssNano() {
+//     return src('./src/css/**/*.css')
+//         .pipe(concat('app.css'))
+//         .pipe(cssNano())
+//         .pipe(dest('./dist'));
+// }
 
 
 module.exports = {
     build: parallel(copyHttml, copyJs, copyCss),
-    uglify: parallel(copyMiniUglifyJs, copyMiniCssNano, copyHttml)
 };
 
-
+// uglify: parallel(copyMiniUglifyJs, copyMiniCssNano, copyHttml)
 
