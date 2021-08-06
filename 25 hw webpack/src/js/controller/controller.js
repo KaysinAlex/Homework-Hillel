e00b5ff62d29fa6fc5af6fbb347de749f55c6185
Kays <kaysin86@gmail.com>
2021-08-06 20:56:14 +0300

    code added to controller , init npm, webpack cli instal gitignore added

--- 25 hw webpack/src/js/controller/controller.js
+++ 25 hw webpack/src/js/controller/controller.js
@@ -1,19 +1,37 @@
+import { TodosView } from './view/todosView.js';
+import { FormTodoView } from './view/todosFormView.js';
+import { TodoCollection } from './module/module.js';
+
 export class TodoController {
     constructor() {
+        console.log('work export?');
         this.todosView = new TodosView({
             onToggle: this.onToggle.bind(this),
             onDelete: (id) => this.onDelete(id)
         });
         this.formView = new FormTodoView({
-            onSave: (todo)=>this.onSave(todo)
-        })
+            onSave: (todo) => this.onSave(todo)
+        });
 
-console.log('work export?')
+        $('container')
+            .prepend(this.todosView.$el)
+            .prepend(this.formView.$el);
+        
+        this.collection = new TodoCollection();
 
-
-
-
-
-
+        this.collection.getListTodo()
+        .then(()=>this.renderListTodo())
     }
+    onToggle(id) {
+        console.log(this.onToggle);
+        this.collection.onToggle(id);
+        this.renderListTodo();
+    }
+    
+    
+    renderListTodo() {
+        console.log('renderListTodo');
+        this.todosView.render(this.collection.listTodos);
+    }
+
 }
