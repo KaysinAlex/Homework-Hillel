a494ffa98858a2eb397accfd4dacf551207dfd62
Kays <kaysin86@gmail.com>
2021-08-06 22:03:05 +0300

    code added to module

--- 25 hw webpack/src/js/module/module.js
+++ 25 hw webpack/src/js/module/module.js
@@ -2,6 +2,36 @@ export class TodoCollection {
     constructor() {
         console.log('collection created');
 
-        this.
+        this.list = [];
+    }
+    getList() {
+        console.log('get list fetch');
+
+        return fetch(TODOS_URL)
+            .then(res => res.json())
+            .then((data) => this.list = data)
+    }
+    toggle(id) {
+        const todoElement = this.list.find(item => item.id == id);
+        todoElement.isDone = !todoElement.isDone;
+        console.log('element isDone', this.todoElement);
+        
+        return fetch(`${TODOS_URL}/${id}`, {
+            metod: 'PUT',
+            body: JSON.stringify(todoElement),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+    }
+
+    delete(id){
+        console.log(id);
+    }
+
+    add(newTodo) {
+        newTodo.isDone = false;
+        console.log(newTodo);
+        
     }
 }
