86ebe6afdf9809e9f41eb57c5e8e083ffc4ecff7
Kays <kaysin86@gmail.com>
2021-08-07 00:22:25 +0300

    code added to view

--- 25 hw webpack/src/js/view/todosView.js
+++ 25 hw webpack/src/js/view/todosView.js
@@ -1,3 +1,5 @@
+import '../../css/style.css';
+
 export class TodosView{
     constructor(config) {
         console.log(config);
@@ -10,17 +12,31 @@ export class TodosView{
             .on('click', '.todo-item', (e) => this.onItemClick(e))
         .on('click', 'delete=btn', (e)=> this.onDeleteClick(e))    
     }
-    
+
     render(list) {
         console.log(list);
+        this.$el.html(list.map(this.getTodoElement).join('\n'));
     }
     getTodoElement(todoItem) {
         console.log(todoItem);
+        return `<div class="todo-item addTodoInput ${todoItem.isDone ? 'done' : ''}" data-todo-id="${todoItem.id}">
+                    ${todoItem.title}
+                    <span class="delete-btn">✘</span>
+                </div>`
     }
     onItemClick(e) {
         console.log(e);
+        const id = $(e.target).data('todoId');
+        this.config.onToggle(id);
     }
     onDeleteClick(e) {
         console.log(e);
+        e.stopPtopagation();
+        const id = $(e.target)
+            .closest('todo=item')
+            .data('todoId');
+        
+        this.config.onDelete(id);
+
     }
 }
--- 25 hw webpack/webpack.config.js
+++ 25 hw webpack/webpack.config.js
@@ -7,4 +7,10 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: {
+            test: /\.css$/,
+            use:(style-loader, css-loader)
+        }
+    }
 };
