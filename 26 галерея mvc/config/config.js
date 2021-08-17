2f51a72930e9a155008dd29b000be754b8b33cec
Kays <kaysin86@gmail.com>
2021-08-17 17:21:39 +0300

    add config

--- 26 галерея mvc/config/config.js
+++
@@ -1,1 +0,0 @@
-const ALBUM_URL = 'https://jsonplaceholder.typicode.com/albums';
---
+++ 26 галерея mvc/src/js/config.js
@@ -0,0 +1,5 @@
+const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
+
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?albumId=';
+
+console.log('config work');
