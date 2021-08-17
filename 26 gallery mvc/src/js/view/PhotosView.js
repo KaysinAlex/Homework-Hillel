2ff960e73e8fe6672220d29b40613487f4eb4cee
Kays <kaysin86@gmail.com>
2021-08-17 19:15:10 +0300

    add code to photosView

--- 26 gallery mvc/src/js/view/PhotosView.js
+++ 26 gallery mvc/src/js/view/PhotosView.js
@@ -8,4 +8,15 @@ export default class PhotosView {
         console.log('init Photos View');
         return $('<div id = "photos" class = "photos nine columns"><div>')
     }
+    renderList(list) {
+        console.log('render Photos List');
+        this.$el.html(
+            list.map((photo) => this.generatePhotoHtml(photo))
+                .join('\n')
+        )
+    }
+    generatePhotoHtml({thumbnailUrl, title}) {
+        console.log('generate Photo Html');
+        return `<img class="photo-item" src = "${thumbnailUrl}" alt ="${title}"/>`;
+    }
 }
