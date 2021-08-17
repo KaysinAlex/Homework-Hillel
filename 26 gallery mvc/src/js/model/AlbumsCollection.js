356f95d31da2dd21d8dd0ce777d0cad7d10a0cd3
Kays <kaysin86@gmail.com>
2021-08-17 19:19:07 +0300

    add albumsCollection code

---
+++ 26 gallery mvc/src/js/model/AlbumsCollection.js
@@ -0,0 +1,10 @@
+import { ALBUMS_URL } from "../config";
+import BaseCollection from "./BaseCollection";
+
+export default class AlbumsCollection extends BaseCollection {
+    constructor() {
+        super();
+        this._url = ALBUMS_URL;
+        console.log('Albums Collection');
+    }
+}
--- 26 gallery mvc/src/js/view/PhotosView.js
+++ 26 gallery mvc/src/js/view/PhotosView.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+
 export default class PhotosView {
     constructor() {
         this.$el = this.initView();
