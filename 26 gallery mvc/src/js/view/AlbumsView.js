440dc920c86b31080111f2fd346c5a7258caca19
Kays <kaysin86@gmail.com>
2021-08-17 19:44:55 +0300

    add render and generate function

--- 26 gallery mvc/src/js/view/AlbumsView.js
+++ 26 gallery mvc/src/js/view/AlbumsView.js
@@ -5,8 +5,9 @@ export default class AlbumsView {
         this.config = config;
         this.$el = this.initView();
 
-       
+        console.log('Init AlbumsView ');       
     }
+    
     initView() {
         console.log('Init Albums View');
          return $('<div id = "albums" class = "albums three columns"></div>')
@@ -14,6 +15,14 @@ export default class AlbumsView {
     }
 
     renderList(list) {
-        console.log(list);
+        console.log('render list');
+        this.$el.html(
+            list.map(this.generateAlbumHtml)
+            .join('\n')
+        )
+    }
+    generateAlbumHtml({ id, title }) {
+        console.log('generate Album Html',title);
+        return `<div class="album-item" data-id = "${id}">${title}</div>`;
     }
 }
