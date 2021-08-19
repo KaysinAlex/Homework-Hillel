8fcc09d4af4e6a08d08d21864167a117d5db5b3e
Kays <kaysin86@gmail.com>
2021-08-19 13:36:09 +0300

    add app  and socket

--- 28 chat circle/app.js
+++ 28 chat circle/app.js
@@ -1,37 +1,44 @@
+const url = 'wss://fep-app.herokuapp.com/';
+const chatMessages = document.getElementById('chatMessages');
+const chatForm = document.forms['chat-forms']
 
 
-const circleEl = document.querySelector('.circle');
+const chatSocket = new ChatSocket(url);
+const chatControler = new FornChatController(chatForm, listMessages, chatSocket);
+chatControler.start();
 
-circleEl.addEventListener('mousedown', onMouseDown);
+// const circleEl = document.querySelector('.circle');
 
-function onMouseDown(e) {
-    console.log(e.target.offsetLeft);
-    circleEl.addEventListener('mousemove', onMouseMove);
-    circleEl.addEventListener('mouseup', onMouseUp);
+// circleEl.addEventListener('mousedown', onMouseDown);
+
+// function onMouseDown(e) {
+//     console.log(e.target.offsetLeft);
+//     circleEl.addEventListener('mousemove', onMouseMove);
+//     circleEl.addEventListener('mouseup', onMouseUp);
     
-}
+// }
 
-function onMouseUp(e) {
-    circleEl.removeEventListener('mousemove', onMouseMove);
-    circleEl.removeEventListener('mouseup', onMouseUp);
+// function onMouseUp(e) {
+//     circleEl.removeEventListener('mousemove', onMouseMove);
+//     circleEl.removeEventListener('mouseup', onMouseUp);
     
-}
+// }
 
 
-function onMouseMove(e) {
-   // move(e.x, e.y);
-    send({
-        type: 'move',
-        payload: {
-            x: e.x,
-            y: e.y,            
-        }
-    })
-}
+// function onMouseMove(e) {
+//    // move(e.x, e.y);
+//     send({
+//         type: 'move',
+//         payload: {
+//             x: e.x,
+//             y: e.y,            
+//         }
+//     })
+// }
 
-function move(x, y) {
-    circleEl.style.left = x -50 + 'px';
-    circleEl.style.top = y - 50 + 'px';
-}
+// function move(x, y) {
+//     circleEl.style.left = x -50 + 'px';
+//     circleEl.style.top = y - 50 + 'px';
+// }
     
-console.log(socket);
+// console.log(socket);
