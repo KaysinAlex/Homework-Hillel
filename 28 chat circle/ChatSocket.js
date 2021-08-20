642cd7c1dc1e614574985351b45e3073d69ea456
Kays <kaysin86@gmail.com>
2021-08-20 14:24:25 +0300

    commit all

--- 28 chat circle/ChatSocket.js
+++ 28 chat circle/ChatSocket.js
@@ -1,52 +1,55 @@
-class ChatSocket{
-    constructor(url) {
-        this.url = url;
-    }
-    createSocketChat(config) {
-        let socket = new WebSocket(this.url);
-        socket.onopen = config.onOpen;
-        socket.onmessage = config.onMessage;
-        socket.onclose = config.onClose;
-        socket.onerror = config.onError;
-        return socket;
-    }
-    sendSocketMessage(socket, message) {
-        if (socket.readyState === WebSocket.OPEN) {
-            socket.send(JSON.stringify(message));
-        }
-    }
-}
 
 
 
+const url = 'wss://fep-app.herokuapp.com/';
+let socket = null;
 
-// const url = 'wss://fep-app.herokuapp.com/';
-// let socket = null;
-
-// function initConnection() {
-//     socket = new WebSocket(url);
-
-//     socket.onopen = () => {
-//         console.log('socket connected');
-//     };
-//     socket.onmessage = (msg) => {
-//         const data = JSON.parse(msg.data);
-//         move(data.payload.x, data.payload.y);
+function initConnection() {
+        socket = new WebSocket(url);
+    
+        socket.onopen = () => {
+                console.log('socket connected');
+            };
+    socket.onmessage = (msg) => {
+            const data = JSON.parse(msg.data);
+        move(data.payload.x, data.payload.y);
         
-//     };
-//     socket.onclose = () => {
-//         initConnection();
-//         console.log('closed');
-//     }
-//     socket.onerror = (err) => {
-//         console.log('🚀 ~ initConnection ~ err', err);
-//     };
-// }
-// function send(msg) {
-//     if (socket.readyState === WebSocket.OPEN) {
-//         socket.send(JSON.stringify(msg));
-//     }
-// }
-
-// initConnection();
-
+    };
+    socket.onclose = () => {
+        initConnection();
+        console.log('closed');
+    }
+    socket.onerror = (err) => {
+            console.log('🚀 ~ initConnection ~ err', err);
+        };
+    }
+    function send(msg) {
+            if (socket.readyState === WebSocket.OPEN) {
+                    socket.send(JSON.stringify(msg));
+                }
+            }
+            
+            initConnection();
+            
+            
+            /* class ChatSocket{
+                constructor(url) {
+                    this.url = url;
+                }
+                initSocketChat(config) {
+                    let socket = new WebSocket(this.url);
+                    socket.onopen = config.onOpen;
+                    socket.onmessage = config.onMessage;
+                    socket.onclose = config.onClose;
+                    socket.onerror = config.onError;
+                    return socket;
+                }
+                sendSocketMessage(socket, message) {
+                    if (socket.readyState === WebSocket.OPEN) {
+                        createCircleMessage();
+                        this.socket.send(JSON.stringify(message));
+                    }
+                }
+                
+            }
+            */
--- 28 chat circle/FormChatController.js
+++ 28 chat circle/FormChatController.js
@@ -1,4 +1,4 @@
-class FormChatController {
+/* class FormChatController {
     constructor(
         chatForm, chatMessagesElement,
         chatSocket
@@ -16,37 +16,25 @@ class FormChatController {
             onClose: this.onSocketClose.bind(this),
             onError: this.onSocketError.bind(this),
         };
-        this.socket = chatSocket.createSocketChat(config);
+        this.socket = chatSocket.initSocketChat(config);
     }
 
     start() {
-        this.chatForm.addEventListener('submit', this.onFormSubmit(this));
+        this.chatForm.addEventListener('onclick',onFormSubmit.bind(this));
     }
 
-    onSocketOpen(event) {
-        let socket = event.target;
-        let message = {
-            type: 'add',
-            payload: {
-                id: 'uniqueID',
-                color: 'red',
-                size: 50 +'px',
-                x: 50 +'px',
-                y: 50+'px',
-            },
-        }
-        this.chatSocket.sendSocketMessage(socket, message);
-        console.log('🚀 ~ FormChatController ~ onSocketOpen ~ socket, message', socket, message);
-        console.log('Отправляем на сервер');
-        
+    onSocketOpen() {
+        this.add();        
     }
 
     onMessage(msg) {
         const message = JSON.parse(msg.data);
+
         console.log('🚀 ~ FormChatController ~ onMessage ~ message', message);
         
-        this.renderMessage(message);
+       
     }
+
     onSocketClose() {
         console.log('close');
         this.initSocket();
@@ -56,44 +44,61 @@ class FormChatController {
 }
     onFormSubmit(event) {
         event.preventDefault();
-        let colorValue = this.getColorValue();
-        let sizeValue = this.getSizeValue();
-        let message = this.readyMessage(colorValue, sizeValue);
-
+        let colorValue = this.color;
+        let sizeValue = this.size;
+        let top = e.pageY - 50;
+        let left = e.pageX - 50;
+        let message = this.update(colorValue, sizeValue,top,left);
+        createCircleMessage();
         this.chatForm.sendSocketMessage(this.socket, message);
 
         this.resetForm();
 
     }
-    renderMessage(message) {
+    
+
+
+    add() {
+        this.socket.send(
+            JSON.stringify({
+                type: 'add',
+                payload: {
+                    id: this.uniqueID,
+                    color: this.color,
+                    size: 100+'px',
+                    top: 100,
+                    left: 100,
+                },
+            }),
+        );
+    }
+
+    update({color,size,top,left}) {
+        this.socket.send(
+            JSON.stringify({
+                type: 'update',
+                payload: {
+                    id: this.uniqueID,
+                    color,
+                    size,
+                    top,
+                    left,
+                },
+            })
+        );
+        this.resetForm();
+    }
+    
+    createCircleMessage(message) {
+
     console.log('🚀 ~ FormChatController ~ renderMessage ~ message', message);
-        this.chatMessagesElement.insertAdjacentHTML('beforeend', `<div class = "circle"> ${message.payload}</div>`);
+        this.chatMessagesElement.insertAdjacentHTML('beforeend', `<div id = "circle"> ${message.payload.id}</div>`);
+
     }
     resetForm() {
         console.log('reset form');
         this.chatForm['input-size'].value = '';
-    }
-    readyMessage(colorValue, sizeValue) {
-        console.log('readyMessage');
-        return {
-            type: 'update',
-            payload:
-            {
-                id:'uniqueID',
-                color: colorValue,
-                size: sizeValue,
-                x: e.x,
-                y: e.y
-            }
-        }
-    }
-    getColorValue() {
-        return this.chatForm.elements['input-color'].value;
-    }
-
-    getSizeValue() {        
-        return this.chatForm.elements['input-size'].value;
-    }
-   
+    }  
 
  }
+ */
