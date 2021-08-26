47adc2dbd6e8455d0192c8cb75cc57c94c6d8506
Kays <kaysin86@gmail.com>
2021-08-26 22:23:03 +0300

    add css style loader and html plugin

---
+++ 28Ballchat/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+         devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 3000,
+    },
+    module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+     
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
